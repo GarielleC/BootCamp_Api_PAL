ff9a4f73f5c0573548606b6df7c2b574b6e04f14
GarielleC <141339259+GarielleC@users.noreply.github.com>
2024-01-17 11:43:51 +0100

    Déplacement des images

--- Backend/routers/router.js
+++ Backend/routers/router.js
@@ -5,9 +5,9 @@ const router = require('express').Router();
 // Importation des routes spécifiques pour chaque fonctionnalité
 // const authRouter = require('./auth.router');
 const bookRouter = require('./book.router');
-// const bookToReadRouter = require('./bookToRead.router');
-// const bookToBuyRouter = require('./bookToBuy.router');
-// const ReadLibraryRouter = require('./ReadLibrary.router');
+const bookToReadRouter = require('./bookToRead.router');
+const bookToBuyRouter = require('./bookToBuy.router');
+const ReadLibraryRouter = require('./ReadLibrary.router');
 
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
@@ -15,11 +15,11 @@ router.use(function timeLog(req, res, next) {
   });
    
 //Utilisation des routes spécifiques avec des préfixes pour créer des chemins complets
-// router.use('/auth', authRouter);
+router.use('/auth', authRouter);
 router.use('/book', bookRouter);
-// router.use('/bookToRead', bookToReadRouter);
-// router.use('/bookToBuy', bookToBuyRouter);
-// router.use('/ReadLibrary', ReadLibraryRouter);
+router.use('/bookToRead', bookToReadRouter);
+router.use('/bookToBuy', bookToBuyRouter);
+router.use('/ReadLibrary', ReadLibraryRouter);
 
 
 
