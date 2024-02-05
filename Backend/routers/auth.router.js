aa43d86081feecc26728ce712b461241873ea045
GarielleC <141339259+GarielleC@users.noreply.github.com>
2024-02-05 12:31:18 +0100

    FonctionnementDBAuth

--- Backend/routers/auth.router.js
+++ Backend/routers/auth.router.js
@@ -1,23 +1,24 @@
 //Importations
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
-const authController = require('../controllers/auth.controller');
-const loginValidator = require('../validator/login.validator');
-const emailValidator = require('../validator/email.validator');
-const authRouter = require('express').Router(); //Permet de crée une nouvelle instance de routeur Express et la stocke dans la variable authRouter
+const authController = require("../controllers/auth.controller");
+const loginValidator = require("../validator/login.validator");
+const emailValidator = require("../validator/email.validator");
+const authRouter = require("express").Router(); //Permet de crée une nouvelle instance de routeur Express et la stocke dans la variable authRouter
 
-
-authRouter.route('/login')
+authRouter
+    .route("/login")
     .post(authController.login)
     .all((req, res) => {
-        res.statusCode(405).send('Unavailable')
+        res.statusCode(405).send("Unavailable");
     });
 
-    authRouter.route('/register')
+authRouter
+    .route("/register")
     .post(loginValidator, emailValidator, authController.register)
     .all((req, res) => {
-        res.statusCode(405).send('Unavailable')
+        res.statusCode(405).send("Unavailable");
     });
 
 module.exports = authRouter;
